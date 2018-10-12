3bf16e3043fc5625ea5803c5d7957f187495de18
Stanchev <astanchev100@gmail.com>
2018-10-12 11:12:03 +0300

    ExportGradesCommand - base added

--- demo-db.core/demo-db.core/Export/ExportToPDF.cs
+++ demo-db.core/demo-db.core/Export/ExportToPDF.cs
@@ -6,54 +6,56 @@ using System.IO;
 using System.Text;
 
 
-namespace demo_db.core.ExportToPDF
+namespace demo_db.core.Export
 {
     public class ExportToPDF
     {
         public static void GeneratePDFReport()
         {
             try
-            {
+            {                
                 PdfPTable footer = new PdfPTable(1);
                 PdfPTable name = new PdfPTable(1);
                 PdfPTable table = new PdfPTable(3);
-                var boldFont = FontFactory.GetFont(FontFactory.HELVETICA_BOLD, 16);
+                var boldFont = FontFactory.GetFont(FontFactory.TIMES_BOLD, 16);
 
+                //Top of the page
                 Chunk c1 = new Chunk("STUDENT REPORT", boldFont);
                 footer.DefaultCell.HorizontalAlignment = Element.ALIGN_CENTER;
                 footer.DefaultCell.Border = 0;
                 footer.AddCell(new Phrase(c1));
 
+                //Change pesho goshev with current username -> fullname
                 Chunk studentName = new Chunk("Pesho Goshev", boldFont);
                 name.DefaultCell.HorizontalAlignment = Element.ALIGN_CENTER;
                 name.DefaultCell.Border = 0;
                 name.AddCell(new Phrase(studentName));
-
+                //Not changeable
                 PdfPCell courses = new PdfPCell(new Phrase("Courses", boldFont));
                 PdfPCell assignments = new PdfPCell(new Phrase("Assignments", boldFont));
-                PdfPCell grades = new PdfPCell(new Phrase("Grades", boldFont));
+                PdfPCell gradesCell = new PdfPCell(new Phrase("Grades", boldFont));
 
+                //Main table - Left column -> courses - right column -> nested tables
                 table.TotalWidth = 400f;
                 table.LockedWidth = true;
                 table.AddCell(courses);
                 table.AddCell(assignments);
-                table.AddCell(grades);
+                table.AddCell(gradesCell);
                 table.AddCell("DSA");
 
-                PdfPTable nestedTabe1 = new PdfPTable(1);
-                PdfPCell nestedCell1 = new PdfPCell(nestedTabe1);
+                //Nested table with Assaignments and Grades
+                PdfPTable nestedTable = new PdfPTable(1);
+                PdfPCell nestedCell1 = new PdfPCell(nestedTable);
                 nestedCell1.Padding = 0f;
-                nestedTabe1.AddCell(new Phrase("Linear"));
-                nestedTabe1.AddCell(new Phrase("Tree"));
+                nestedTable.AddCell(new Phrase("Linear"));
+                nestedTable.AddCell(new Phrase("2"));
+                table.AddCell(nestedCell1);
+                
+                nestedTable.AddCell(new Phrase("Tree"));
+                nestedTable.AddCell(new Phrase("32"));
                 table.AddCell(nestedCell1);
 
-                PdfPTable nestedTable2 = new PdfPTable(1);
-                PdfPCell nestedCell2 = new PdfPCell(nestedTable2);
-                nestedCell2.Padding = 0f;
-                nestedTable2.AddCell(new Phrase("32"));
-                nestedTable2.AddCell(new Phrase("2"));
-                table.AddCell(nestedCell2);
-
+                //Set up file name and directory
                 string folderPath = ".\\PDF\\";
                 if (!Directory.Exists(folderPath))
                 {
