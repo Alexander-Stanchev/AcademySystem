d6fb301c8c380ea90508597d1b40466c342182f0
Ali Teber <ateber91@outlook.com>
2018-10-16 02:56:03 +0300

    RegisterUserCommand tests added.

--- demo-db.core/demo-db.Tests/RegisterUserCommandTest.cs
+++ demo-db.core/demo-db.Tests/RegisterUserCommandTest.cs
@@ -11,79 +11,77 @@ using System.Text;
 namespace demo_db.Tests
 {
     [TestClass]
-    class RegisterUserCommandTest
+    public class RegisterUserCommandTest
     {
         [TestMethod]
-        public void ExecuteShouldRetrurnMessageWhenUserNotLogged()
+        public void ExecuteShouldRetrurnMessageWhenUserLogged()
         {
             // Arrange 
             var state = new Mock<ISessionState>();
             var builder = new Mock<IStringBuilderWrapper>();
             var service = new Mock<IUserService>();
 
-            var command = new UpdateStudentToTeacherCommand(state.Object, builder.Object, service.Object);
+            var command = new RegisterUserCommand(state.Object, builder.Object, service.Object);
+
+            state.SetupGet(m => m.IsLogged).Returns(true);
+
+            var parameters = new string[] { "Username", "pass", "First", "Last" };
+
+            //Assert + Act
+            Assert.AreEqual("You are already logged.", command.Execute(parameters));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ExecuteShouldThrowExceptionWhenLessThan4ParametersPassed()
+        {
+            // Arrange 
+            var state = new Mock<ISessionState>();
+            var builder = new Mock<IStringBuilderWrapper>();
+            var service = new Mock<IUserService>();
+
+            var command = new RegisterUserCommand(state.Object, builder.Object, service.Object);
 
             state.SetupGet(m => m.IsLogged).Returns(false);
+            
 
-            var parameters = new string[] { "UserName", "1" };
-
-            //Assert + Act
-            Assert.AreEqual("You have to log in first.", command.Execute(parameters));
-        }
-
-        [TestMethod]
-        public void ExecuteShouldRetrurnMessageWhenIncorrectRolePassed()
-        {
-            // Arrange 
-            var state = new Mock<ISessionState>();
-            var builder = new Mock<IStringBuilderWrapper>();
-            var service = new Mock<IUserService>();
-
-            var command = new UpdateStudentToTeacherCommand(state.Object, builder.Object, service.Object);
-
-            state.SetupGet(m => m.IsLogged).Returns(true);
-            state.SetupGet(m => m.RoleId).Returns(2);
-
-            var parameters = new string[] { "UserName" };
-
-            //Assert + Act
-            Assert.AreEqual("You dont have access.", command.Execute(parameters));
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void ExecuteShouldThrowExceptionWhenNoUserNamePassed()
-        {
-            // Arrange 
-            var state = new Mock<ISessionState>();
-            var builder = new Mock<IStringBuilderWrapper>();
-            var service = new Mock<IUserService>();
-
-            var command = new UpdateStudentToTeacherCommand(state.Object, builder.Object, service.Object);
-
-            state.SetupGet(m => m.IsLogged).Returns(true);
-            state.SetupGet(m => m.RoleId).Returns(1);
-
-            var parameters = new string[] { "" };
+            var parameters = new string[] { "username", "1234", "asd" };
 
             var result = command.Execute(parameters);
         }
 
         [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void ExecuteShouldThrowExceptionWhenMoreThanOneParametersPassed()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExecuteShouldThrowExceptionWhenUsernameIsNull()
         {
             // Arrange 
             var state = new Mock<ISessionState>();
             var builder = new Mock<IStringBuilderWrapper>();
             var service = new Mock<IUserService>();
 
-            var command = new UpdateStudentToTeacherCommand(state.Object, builder.Object, service.Object);
+            var command = new RegisterUserCommand(state.Object, builder.Object, service.Object);
 
-            state.SetupGet(m => m.IsLogged).Returns(true);
-            state.SetupGet(m => m.RoleId).Returns(1);
+            state.SetupGet(m => m.IsLogged).Returns(false);
+            
+            var parameters = new string[] { "", "1234", "First", "Last"};
 
-            var parameters = new string[] { "username", "1234" };
+            var result = command.Execute(parameters);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ExecuteShouldThrowExceptionWhenPasswordIsNull()
+        {
+            // Arrange 
+            var state = new Mock<ISessionState>();
+            var builder = new Mock<IStringBuilderWrapper>();
+            var service = new Mock<IUserService>();
+
+            var command = new RegisterUserCommand(state.Object, builder.Object, service.Object);
+
+            state.SetupGet(m => m.IsLogged).Returns(false);
+
+            var parameters = new string[] { "userName", "", "First", "Last" };
 
             var result = command.Execute(parameters);
         }
